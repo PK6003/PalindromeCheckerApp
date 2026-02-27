5accfae9c39166b40c6b1c928e626e747be53231
PK6003 <piyush.20mm@gmail.com>
2026-02-27 15:52:47 +0530

    UC2 COMPLEDTED

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheckerApp {
+    static void main(String[] args) {
+        String original = "madam";
+        String reversed = "";
+
+        for (int i = original.length() -1 ; i>=0 ; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("THE STRING\"" + original + "\" IS A PALINDROME");
+        }else{
+            System.out.println("THE STRING\"" + original + "\" IS NOT A PALINDROME");
+        }
+
+        System.out.println("Original: " + original);
+        System.out.println("Reversed :" + reversed);
+
+    }
 }
