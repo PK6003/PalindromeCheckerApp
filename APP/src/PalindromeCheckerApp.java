aecf700df95aaf3083a944aac274ed98771e218b
PK6003 <piyush.20mm@gmail.com>
2026-03-16 15:00:53 +0530

    UC8

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,33 +1,83 @@
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class PalindromeCheckerApp {
+
+    // Node class for singly linked list
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
     public static void main(String[] args) {
 
         String text = "madam";
-        Deque<Character> deque = new LinkedList<>();
 
-        // Insert all characters into deque
+        // Convert string to linked list
+        Node head = null, tail = null;
         for (int i = 0; i < text.length(); i++) {
-            deque.addLast(text.charAt(i));
-        }
-
-        boolean isPalindrome = true;
-
-        // Compare front and rear characters
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
+            Node newNode = new Node(text.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
         }
 
-        // Print result
-        if (isPalindrome) {
+        // Check palindrome
+        if (isPalindrome(head)) {
             System.out.println("THE STRING \"" + text + "\" IS A PALINDROME");
         } else {
             System.out.println("THE STRING \"" + text + "\" IS NOT A PALINDROME");
         }
+    }
+
+    // Function to check palindrome using linked list
+    static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+        // Find middle using slow and fast pointers
+        Node slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverse(slow.next);
+
+        // Compare first and second halves
+        Node firstHalf = head;
+        Node tempSecond = secondHalf;
+        boolean palindrome = true;
+        while (tempSecond != null) {
+            if (firstHalf.data != tempSecond.data) {
+                palindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            tempSecond = tempSecond.next;
+        }
+
+        // Optional: Restore the list (not required here)
+        slow.next = reverse(secondHalf);
+
+        return palindrome;
+    }
+
+    // Reverse linked list function
+    static Node reverse(Node head) {
+        Node prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
