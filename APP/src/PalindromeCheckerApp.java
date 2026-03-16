e7bb0f4ac95160642529497c3df6224259442daa
PK6003 <piyush.20mm@gmail.com>
2026-03-16 14:54:58 +0530

    UC6

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
@@ -5,23 +7,28 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         String text = "madam";
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
+        // Add characters to queue (FIFO) and stack (LIFO)
         for (int i = 0; i < text.length(); i++) {
-            stack.push(text.charAt(i));
+            char ch = text.charAt(i);
+            queue.add(ch);   // enqueue
+            stack.push(ch);  // push
         }
 
         boolean isPalindrome = true;
 
-        // Pop and compare
-        for (int i = 0; i < text.length(); i++) {
-            if (text.charAt(i) != stack.pop()) {
+        // Compare dequeue vs pop
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
         }
 
+        // Print result
         if (isPalindrome) {
             System.out.println("THE STRING \"" + text + "\" IS A PALINDROME");
         } else {
